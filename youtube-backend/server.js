84d09a82280159edccbaaea12a0d39d6ccfb3933
Sahil-Bhowmick <104313482+Sahil-Bhowmick@users.noreply.github.com>
2025-04-09 21:39:51 +0530

    Update server.js

--- youtube-backend/server.js
+++ youtube-backend/server.js
@@ -18,7 +18,7 @@ connectDB();
 
 // Middleware
 app.use(helmet());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://youtube-clone-frontend-p90d.onrender.com", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
