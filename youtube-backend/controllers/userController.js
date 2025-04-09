6e805b2d745caf5a837febdd54eebb1c193baab6
Sahil Bhowmick <sahilbhowmick1@gmail.com>
2025-04-09 21:08:27 +0530

    Changing mongo url

--- youtube-backend/controllers/userController.js
+++ youtube-backend/controllers/userController.js
@@ -9,10 +9,8 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 const cookieOptions = {
   httpOnly: true,
-  secure: false, // In development, secure should be false
-  sameSite: "Lax", // Lax works fine for development
-  // secure: process.env.NODE_ENV === "production", // Set secure cookies in production
-  // sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+  secure: process.env.NODE_ENV === "production", // Set secure cookies in production
+  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
 
