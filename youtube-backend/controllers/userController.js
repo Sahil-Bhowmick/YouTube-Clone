7327655a6f68f8c695a4e739ba6b80a7f817df89
Sahil-Bhowmick <104313482+Sahil-Bhowmick@users.noreply.github.com>
2025-04-15 12:36:32 +0530

    Update userController.js

--- youtube-backend/controllers/userController.js
+++ youtube-backend/controllers/userController.js
@@ -17,7 +17,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
 // };
 const cookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production", // True in production (HTTPS)
+  secure: process.env.NODE_ENV === "production",
   sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
